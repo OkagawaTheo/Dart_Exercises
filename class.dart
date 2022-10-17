fe3a022cf48bb2f76df4552a45e41cc6f2ab995c
Theo Okagawa Rodrigues <okagawatheo@gmail.com>
2022-10-17 18:52:53 -0300

    atribui parâmetros diretamente no call da função

--- class.dart
+++ class.dart
@@ -1,16 +1,19 @@
 void main(){
-Pessoa pessoa_theo = new Pessoa();
-pessoa_theo.nome = 'Theo';
-pessoa_theo.idade = 16;
-
-pessoa_theo.apresenta();
+  Pessoa pessoa1 = new Pessoa('Theo', null); // instanciar por esses parâmetros
+  // pessoa1.nome = 'Marcos';
+  //pessoa1.idade = 12; 
+  pessoa1.ApresentarNome();
 }
 
-class Pessoa {
+
+class Pessoa { // nome + idade e apresentar
   String? nome;
   int? idade;
 
-  void apresenta() => {
-    print('meu nome é $nome')
+  Pessoa(this.nome, this.idade); // obrig. as das duas var
+
+  void ApresentarNome() => {
+
+    print('Meu nome é $nome e eu tenho $idade anos.') // nome que está sendo atribuido a pessoa1
   };
 }
