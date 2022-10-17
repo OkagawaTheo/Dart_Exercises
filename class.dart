ff40ff190b71e369330eeda6dcac0723bfab1674
Theo Okagawa Rodrigues <okagawatheo@gmail.com>
2022-10-17 19:46:13 -0300

    resolvi bug que o void envelhecer nao funcionava e adicionei mais um parâmetro

--- class.dart
+++ class.dart
@@ -1,19 +1,27 @@
-void main(){
-  Pessoa pessoa1 = new Pessoa('Theo', null); // instanciar por esses parâmetros
+
+
+void main() {
+  Pessoa pessoa1 = new Pessoa('Theo', 16, 16); // instanciar por esses parâmetros
   // pessoa1.nome = 'Marcos';
-  //pessoa1.idade = 12; 
+  // pessoa1.idade = 12; 
+  pessoa1.Envelhecer();
   pessoa1.ApresentarNome();
+  
 }
 
 
 class Pessoa { // nome + idade e apresentar
   String? nome;
   int? idade;
+  int? idadeEnv;
 
-  Pessoa(this.nome, this.idade); // obrig. as das duas var
+  Pessoa(this.nome, this.idade, this.idadeEnv); // obrig. as das duas var
+
+void Envelhecer () => {
+  idadeEnv = this.idade! + 1, 
+  };
 
   void ApresentarNome() => {
-
-    print('Meu nome é $nome e eu tenho $idade anos.') // nome que está sendo atribuido a pessoa1
+    print('Meu nome é ${this.nome} e eu tenho ${this.idade} anos. Ano que vem farei ${this.idadeEnv} ') // nome que está sendo atribuido a pessoa1
   };
 }
